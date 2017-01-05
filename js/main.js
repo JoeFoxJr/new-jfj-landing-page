7010b9efd4006dc0e2db4cbc72a05ed1b67a14aa
Joe Fox Jr <joefoxjr@gmail.com>
2017-01-05 18:43:00 -0500

    continue adding content

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
-// Add class to #top-nav-bar upon scrolling
+
 $(document).ready(function() {
+
+  // Add class to #top-nav-bar upon scrolling
   $(window).scroll(function() {
+
     if ($(window).scrollTop() > 1) {
-      $('#top-nav-bar').addClass('scrolled');
+      $('#top-nav-bar').addClass('scrolled-mid');
     } else {
-      $('#top-nav-bar').removeClass('scrolled');
+      $('#top-nav-bar').removeClass('scrolled-mid');
     };
+
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      $('#top-nav-bar').addClass('scrolled-bottom');
+    } else {
+      $('#top-nav-bar').removeClass('scrolled-bottom');
+    };
+
   });
+
+
+  // Enable smooth scrolling for each same-page anchor
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+    });
+
 });
 
 
